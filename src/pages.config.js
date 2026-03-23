4d63f313770532c7c318773a97ae5e8bbdfe4a2d
ByTheWay0716 <btw180032464@gmail.com>
2026-03-23 17:49:22 -0400

    Update src/pages.config.js with Home and GamePlayer page imports and exports PAGES and pagesConfig configuration

---
+++ src/pages.config.js
@@ -0,0 +1,13 @@
+// Page imports
+import Home from './Home';
+import GamePlayer from './GamePlayer';
+
+// Pages configuration
+export const PAGES = {
+  Home,
+  GamePlayer,
+};
+
+export const pagesConfig = {
+  // Add your configuration options here
+};
